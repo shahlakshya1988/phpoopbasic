65491b8f25395d1c337077c4bd1ce36c76c2cc82
Shah Lakshya Pradhyuman <shahlakshya1988@gmail.com>
2019-11-04 23:48:23 +0530

    25. Encapsulation

---
+++ example18.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Encapsulation
+ * Class is representation of the object, object hides the details of the implementation
+ * from the users of the object. 
+ * Refers to protecting the internal code from the extenal code
+ * Hiding the details of implementation
+ * 1) change implementation any tim
+ * 2) Object of one class can't change properties of other class
+ */
