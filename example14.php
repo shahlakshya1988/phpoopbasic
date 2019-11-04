43c91bce8b837d6dd38a8405d7e9877c99153309
Lakshya Shah <shahlakshya1988@gmail.com>
2019-11-04 17:38:36 +0530

    20. Interface

---
+++ example14.php
@@ -0,0 +1,18 @@
+<?php 
+/**
+ * Interface, group of classes not related with
+ * extends, jar and door can be opened or closed
+ * but they are not related, but both they can 
+ * be opened or closed
+ * Interface specify that object is capable of 
+ * performing certain actions, but does not tell
+ * how it will perform
+ * WE CAN ONLY DECELARE METHODS, BUT WE CAN'T DEFINE THE METHOD
+ * interface Name_of_interface{
+ *		method 1
+ *		...
+ *		...
+ *		..
+ *		method 2
+ * }
+ */
